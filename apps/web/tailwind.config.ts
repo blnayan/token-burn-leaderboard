2d120030f40f62a97219415d55dc6b3046e49336
Nayan Bhut <52083057+blnayan@users.noreply.github.com>
2026-06-01 00:04:32 +0000

    fix: add web eslint cli config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } satisfies Config;
 
 export default config;
